b2eedebe18fdb76719497701ef5e18aa774160e6
happs <happs@blue>
2013-04-07 23:33:52 -0400

    faster rand gen

--- rand.hpp
+++ rand.hpp
@@ -5,6 +5,7 @@
 #define MIN(x,y) ((x) < (y) ? (x) : (y))
 
 std::random_device rd;
+std::mt19937 gen(rd());
 std::uniform_real_distribution<> uniform_dist(0,1);
 
 template<intmax_t T1,intmax_t T2>
@@ -12,7 +13,7 @@ int randomLevel(int n, const std::ratio<T1,T2> &r)
 {
 	
 	int newLevel = 1;
-	while(uniform_dist(rd) < (double)r.num / r.den && newLevel < n)
+	while(uniform_dist(gen) < (double)r.num / r.den && newLevel < n)
 		++newLevel;
 
 	return MIN(newLevel,n);
